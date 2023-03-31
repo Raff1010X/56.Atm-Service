3e87493823cb487efde9c683507009527c7249c6
Rafał Kowalczyk <kowalczykr@trendglass.pl>
2023-03-31 12:54:41 +0200

    add: atmservice model

--- models/atms.js
+++ models/atms.js
@@ -1,4 +1,22 @@
-exports.calculateOrder = (data) => {
-    
-    return data;
+exports.calculateOrder = (data) =>
+    data.sort(compareRequest).map(mapRequest).filter(filterRequest);
+
+const compareRequest = (a, b) => {
+    return a.region !== b.region
+        ? a.region - b.region
+        : order[a.requestType] - order[b.requestType];
+};
+
+const mapRequest = (el) => {
+    return { region: el.region, atmId: el.atmId };
+};
+
+const filterRequest = (val, ind, arr) =>
+    arr.findIndex((el) => el.region === val.region && el.atmId === val.atmId) === ind;
+
+const order = {
+    FAILURE_RESTART: 0,
+    PRIORITY: 1,
+    SIGNAL_LOW: 2,
+    STANDARD: 3,
 };

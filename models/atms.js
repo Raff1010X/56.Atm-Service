32c11ffffe3e60b91531361d5e6c383995690411
Rafał Kowalczyk <raff1410X@gmail.com>
2023-03-31 19:13:37 +0200

    update: atms model - unreadable style ;-)

--- models/atms.js
+++ models/atms.js
@@ -1,22 +1,6 @@
-exports.calculateOrder = (data) =>
-    data.sort(s).map(m).filter(f);
-
-const s = (a, b) => {
-    return a.region !== b.region
-        ? a.region - b.region
-        : order[a.requestType] - order[b.requestType];
-};
-
-const m = (e) => {
-    return { region: e.region, atmId: e.atmId };
-};
-
-const f = (v, i, a) =>
-    a.findIndex((e) => e.region === v.region && e.atmId === v.atmId) === i;
-
-const order = {
-    FAILURE_RESTART: 0,
-    PRIORITY: 1,
-    SIGNAL_LOW: 2,
-    STANDARD: 3,
-};
+// Unredable style ;-)
+exports.calculateOrder = (data) => data.sort(s).map(m).filter(f);
+const s = (a, b) => a.region !== b.region ? a.region - b.region : o[a.requestType] - o[b.requestType];
+const m = (e) => ({ region: e.region, atmId: e.atmId });
+const f = (v, i, a) => a.findIndex((e) => e.region === v.region && e.atmId === v.atmId) === i;
+const o = {FAILURE_RESTART: 0, PRIORITY: 1, SIGNAL_LOW: 2, STANDARD: 3 };
